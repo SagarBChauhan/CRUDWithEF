561bda8400cdfae1a7737da82b2524fcc30121ed
SagarBChauhan <Chuahansagar373@gmail.com>
2020-10-09 15:35:40 +0530

    first commit

---
+++ CRUDWithEF/Controllers/EmpController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDWithEF.Models;
+
+namespace CRUDWithEF.Controllers
+{
+    public class EmpController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        //
+        // GET: /Emp/
+
+        public ActionResult Index()
+        {
+            return View(db.Emps.ToList());
+        }
+
+        //
+        // GET: /Emp/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Emp emp = db.Emps.Single(e => e.EmployeeID == id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        //
+        // GET: /Emp/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Emp/Create
+
+        [HttpPost]
+        public ActionResult Create(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Emps.AddObject(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(emp);
+        }
+
+        //
+        // GET: /Emp/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Emp emp = db.Emps.Single(e => e.EmployeeID == id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        //
+        // POST: /Emp/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Emps.Attach(emp);
+                db.ObjectStateManager.ChangeObjectState(emp, EntityState.Modified);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        //
+        // GET: /Emp/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Emp emp = db.Emps.Single(e => e.EmployeeID == id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        //
+        // POST: /Emp/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Emp emp = db.Emps.Single(e => e.EmployeeID == id);
+            db.Emps.DeleteObject(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUDWithEF/Models/Emp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CRUDWithEF.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Emp
+    {
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class EmployeeMetaData
+    {
+        [Required(ErrorMessage="Name can't be empty")]
+        [StringLengthAttribute(10,MinimumLength=3)]
+        public string Name { get; set; }
+        [Range(20,50)]
+        public int Age { get; set; }
+        [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")]
+         [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+        [Display(Name="Personal Website")]
+        [DataType(DataType.Url)]
+        [DisplayFormat(NullDisplayText = "Personal website not specified")]
+        public string PersonalWebsite { get; set; }
+        [DataType(DataType.Date)]
+       
+        //[DisplayFormat(DataFormatString="{0:dd/MM/yyyy HH:mm:ss}")]
+        public DateTime HireDate { get; set; }
+    }
+}
